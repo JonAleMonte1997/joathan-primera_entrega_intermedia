4013d80039b4bd1cfaf40d3d9a695df318e1b779
Jonathan Alexis Montenegro <63118548+JonAleMonte1997@users.noreply.github.com>
2022-03-29 17:13:46 -0300

    segunda entrega

--- src/main/java/com/jam/projects/appmedica/exceptions/RestExceptionHandler.java
+++ src/main/java/com/jam/projects/appmedica/exceptions/RestExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.jam.projects.appmedica.exceptions;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.naming.SizeLimitExceededException;
 import javax.persistence.EntityNotFoundException;
 import java.util.Date;
 
@@ -22,6 +25,24 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException ex) {
 
         return buildResponseEntity(HttpStatus.BAD_REQUEST, ex);
+    }
+
+    @ExceptionHandler
+    protected ResponseEntity<ErrorResponse> handleMaxSizeListExceededException(MaxSizeListExceededException ex) {
+
+        return buildResponseEntity(HttpStatus.BAD_REQUEST, ex);
+    }
+
+    @ExceptionHandler
+    protected ResponseEntity<ErrorResponse> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex) {
+
+        return buildResponseEntity(HttpStatus.NOT_FOUND, ex);
+    }
+
+    @ExceptionHandler
+    protected ResponseEntity<ErrorResponse> handleException(Exception ex) {
+
+        return buildResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, ex);
     }
 
     private ResponseEntity<ErrorResponse> buildResponseEntity(HttpStatus status, Exception ex) {
